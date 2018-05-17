556937f77eaac21825ecdbec86314a0074769e04
Márton Münz <Marton.Muenz@icr.ac.uk>
2018-05-17 19:39:26 +0200

    Added example unit test code to template directory

---
+++ template/test/unit/test_example.py
@@ -0,0 +1,22 @@
+
+from unittest import TestCase
+import pytest
+
+
+class TestExample(TestCase):
+
+
+    def setUp(self):
+
+        pass
+
+
+    def tearDown(self):
+
+        pass
+
+
+    def test_some_method(self):
+
+        self.assertTrue(1==1)
+
